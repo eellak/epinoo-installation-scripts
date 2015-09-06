48783305abec0b514f9439bd35ed0e538b838e09
Athanasios Douitsis <aduitsis@cpan.org>
2015-09-06 19:37:26 +0300

    more packages in essentials

--- 00-essentials.pp
+++ 00-essentials.pp
@@ -1,4 +1,4 @@
-package { [ 'build-essential' , 'dh-make' , 'bzr-builddeb' ]:
+package { [ 'build-essential' , 'dh-make' , 'bzr-builddeb' , 'apt-get' , 'install' , 'build-essential' , 'git-core' , 'checkinstall' , 'yasm' , 'texi2html' , 'libvorbis-dev' , 'libx11-dev' , 'libvpx-dev' , 'libxfixes-dev' , 'zlib1g-dev' , 'pkg-config' , 'netcat' , 'libncurses5-dev' ]:
   ensure  => installed,
 }
 
