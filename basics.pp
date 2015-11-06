f0ee54e7b2418f6a3fcaaacc3d82a60c7ade48da
Achilleas Pipinellis <axilleas@axilleas.me>
2015-11-06 21:35:47 +0200

    Export ssh keys in own manifest

--- basics.pp
+++ basics.pp
@@ -1,54 +1,14 @@
-
-ssh_authorized_key { 'aduitsis':
-	user	=> 'root',  
-	type	=> 'ssh-rsa',
-	key	=> 'AAAAB3NzaC1yc2EAAAABIwAAAQEAq0WIhNJEEIgPgoqs9szy9i7EzjdJCcYE5YpZE7ufflB2MF+t5+jZ6QyWyNpUp5CkdfCtK//jEGIVx9LK/4tURL44nJuR4tw875gObrSRIU6ZJ6CJRAa3D/9p6T9UPju5BtrIrUaSkIu/OxYr2m1GrJEDbvkZfIRWpVUZFiR54IpKLQDlQfOr3zZ9lp8NobJd50OPO1VnNj9/r1Ij85Vpn+X2P9MLyiOX9tPcXo1pGwGRDB1PFcqSb8sZ+IwBmiGIE0qEx6TURoVOZNdi2s1c+debgzgR+HFPos2a4fhZb/S0yEmWfufzVljgn7XmMcxMOaS6SSHlI9m3r0cjPzgDGw=='
-}
-
-
-ssh_authorized_key { 'vhatzi':
-	user	=> 'root',  
-	type	=> 'ssh-rsa',
-	key	=> '1yc2EAAAABIwAAAQEAq0WIhNJEEIgPgoqs9szy9i7EzjdJCcYE5YpZE7ufflB2MF+t5+jZ6QyWyNpUp5CkdfCtK//jEGIVx9LK/4tURL44nJuR4tw875gObrSRIU6ZJ6CJRAa3D/9p6T9UPju5BtrIrUaSkIu/OxYr2m1GrJEDbvkZfIRWpVUZFiR54IpKLQDlQfOr3zZ9lp8NobJd50OPO1VnNj9/r1Ij85Vpn+X2P9MLyiOX9tPcXo1pGwGRDB1PFcqSb8sZ+IwBmiGIE0qEx6TURoVOZNdi2s1c+debgzgR+HFPos2a4fhZb/S0yEmWfufzVljgn7XmMcxMOaS6SSHlI9m3r0cjPzgDGw=='
-}
-
-ssh_authorized_key { 'angela':
-	user	=> 'root',  
-	type	=> 'ssh-rsa',
-	key	=> 'AAAAB3NzaC1yc2EAAAABIwAAAQEAzkR6sRjsXUltrFSWNrvGpNy/t/WTY2yIwSmWWRSXJswxu4o/N/wZVVDBCJ3plTV6Ite9t9VnlYPYgqDM5QvFz41tTSl8aiJYJqdmX5/ysUyM5CAWDxgQZK5ih08IFL03Kz9q/M031aeU5zLrXz81ylYeG2cTRpZPPGdFfUyQzJcnvk4cNFPfF4QPC0LEMyYLycUFZho8vvkWhRivvVop2xZDSnpLTFbR+6OEoJ5Ro/uUrCcgGTRCGIVkWHBvduj/6TT4viPmYx12vS3mspeV9dDSSYb+HLNIf6/p13qrhqkIbFNnqFQi+ekQ4c4+BAA81cd9Zj0ExaKIo7nTBZsuCw=='
-}
-
-
-ssh_authorized_key { 'aduitsis-user':
-	user	=> 'user',  
-	type	=> 'ssh-rsa',
-	key	=> 'AAAAB3NzaC1yc2EAAAABIwAAAQEAq0WIhNJEEIgPgoqs9szy9i7EzjdJCcYE5YpZE7ufflB2MF+t5+jZ6QyWyNpUp5CkdfCtK//jEGIVx9LK/4tURL44nJuR4tw875gObrSRIU6ZJ6CJRAa3D/9p6T9UPju5BtrIrUaSkIu/OxYr2m1GrJEDbvkZfIRWpVUZFiR54IpKLQDlQfOr3zZ9lp8NobJd50OPO1VnNj9/r1Ij85Vpn+X2P9MLyiOX9tPcXo1pGwGRDB1PFcqSb8sZ+IwBmiGIE0qEx6TURoVOZNdi2s1c+debgzgR+HFPos2a4fhZb/S0yEmWfufzVljgn7XmMcxMOaS6SSHlI9m3r0cjPzgDGw=='
-}
-
-
-ssh_authorized_key { 'vhatzi-user':
-	user	=> 'user',  
-	type	=> 'ssh-rsa',
-	key	=> '1yc2EAAAABIwAAAQEAq0WIhNJEEIgPgoqs9szy9i7EzjdJCcYE5YpZE7ufflB2MF+t5+jZ6QyWyNpUp5CkdfCtK//jEGIVx9LK/4tURL44nJuR4tw875gObrSRIU6ZJ6CJRAa3D/9p6T9UPju5BtrIrUaSkIu/OxYr2m1GrJEDbvkZfIRWpVUZFiR54IpKLQDlQfOr3zZ9lp8NobJd50OPO1VnNj9/r1Ij85Vpn+X2P9MLyiOX9tPcXo1pGwGRDB1PFcqSb8sZ+IwBmiGIE0qEx6TURoVOZNdi2s1c+debgzgR+HFPos2a4fhZb/S0yEmWfufzVljgn7XmMcxMOaS6SSHlI9m3r0cjPzgDGw=='
-}
-
-ssh_authorized_key { 'angela-user':
-	user	=> 'user',  
-	type	=> 'ssh-rsa',
-	key	=> 'AAAAB3NzaC1yc2EAAAABIwAAAQEAzkR6sRjsXUltrFSWNrvGpNy/t/WTY2yIwSmWWRSXJswxu4o/N/wZVVDBCJ3plTV6Ite9t9VnlYPYgqDM5QvFz41tTSl8aiJYJqdmX5/ysUyM5CAWDxgQZK5ih08IFL03Kz9q/M031aeU5zLrXz81ylYeG2cTRpZPPGdFfUyQzJcnvk4cNFPfF4QPC0LEMyYLycUFZho8vvkWhRivvVop2xZDSnpLTFbR+6OEoJ5Ro/uUrCcgGTRCGIVkWHBvduj/6TT4viPmYx12vS3mspeV9dDSSYb+HLNIf6/p13qrhqkIbFNnqFQi+ekQ4c4+BAA81cd9Zj0ExaKIo7nTBZsuCw=='
-}
-
-package { [ 
+package { [
 	'module-assistant',
-	'wget', 
-	'git', 
-	'vim', 
-	'make', 
+	'wget',
+	'git',
+	'vim',
+	'make',
 	'tcpdump',
-	'mtr', 
-	'bridge-utils', 
+	'mtr',
+	'bridge-utils',
 	'augeas-tools' ,
-	'pstack', 
+	'pstack',
 	'socat',
 	'dnsutils',
 	'lsof',
@@ -57,7 +17,7 @@ package { [
 	'iftop',
 	'iotop',
 	'lftp',
-	'ca-certificates' , 
+	'ca-certificates' ,
 	'ca-certificates-java',
 	'screen',
 	'tmux',
@@ -65,4 +25,4 @@ package { [
 	'htop',
 	]:
 	ensure	=> present,
-} 
+}
