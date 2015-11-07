bb56016ab6d228c5b37e089aa623f39e8ce6610c
Achilleas Pipinellis <axilleas@axilleas.me>
2015-11-07 12:32:23 +0200

    Remove vars that default to true

--- mysql.pp
+++ mysql.pp
@@ -1,6 +1,4 @@
 class { '::mysql::server':
-  create_root_user        => true,
-  create_root_my_cnf      => true,
   root_password           => $db_root_passwd,
   remove_default_accounts => true,
 }
