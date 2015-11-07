0c8d65d6f7233d83f8756fb05e7364fe993b4b1a
Achilleas Pipinellis <axilleas@axilleas.me>
2015-11-07 13:10:15 +0200

    Revert moodle wwwroot variable setting

--- moodle.pp
+++ moodle.pp
@@ -46,6 +46,6 @@ exec { 'install_moodle':
   cwd       => "$moodle_root/admin/cli",
   creates   => "$moodle_root/config.php",
   path      => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
-  command   => "php install.php --chmod=0750 --lang=el --wwwroot=$moodle_root --dataroot=$data_root --dbuser=moodle --dbpass='$moodle_db_pwd' --fullname='$moodle_fullname' --shortname='$moodle_shortname' --non-interactive --agree-license --adminpass='$moodle_adminpass'",
+  command   => "php install.php --chmod=0750 --lang=el --wwwroot=$moodle_url --dataroot=$data_root --dbuser=moodle --dbpass='$moodle_db_pwd' --fullname='$moodle_fullname' --shortname='$moodle_shortname' --non-interactive --agree-license --adminpass='$moodle_adminpass'",
   user      => 'www-data',
 }
