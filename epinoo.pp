28fa1f61c0afa35fa420ac7814860b41be4dab76
Achilleas Pipinellis <axilleas@axilleas.me>
2015-11-07 12:01:45 +0200

    Add missing comma

--- epinoo.pp
+++ epinoo.pp
@@ -16,7 +16,7 @@ mysql::db { 'epinoo':
 
 file { "$epinoo_root/wp-config.php":
   ensure => present,
-  content => template('wp-config.php.erb')
+  content => template('wp-config.php.erb'),
   mode    => '0600',
 }
 
