6ccebb89537e2f57072de42b770c7e035168bb3e
Achilleas Pipinellis <axilleas@axilleas.me>
2015-11-07 12:20:30 +0200

    Update mysql manifest to create .my.cnf and change root passwd

--- mysql.pp
+++ mysql.pp
@@ -1,4 +1,6 @@
-
-
-class { '::mysql::server': 
+class { '::mysql::server':
+  create_root_user        => true,
+  create_root_my_cnf      => true,
+  root_password           => $db_root_passwd,
+  remove_default_accounts => true,
 }
