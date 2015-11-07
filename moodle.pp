7168e717580939ba065f5c0dcdff0ac0ffbbbd9d
Achilleas Pipinellis <axilleas@axilleas.me>
2015-11-07 12:52:56 +0200

    Fix wrong moodle root path

--- moodle.pp
+++ moodle.pp
@@ -3,7 +3,7 @@ $moodle_source = '/var/tmp/moodle'
 $data_root = '/var/www/moodle-data'
 
 vcsrepo { $moodle_source:
-  
+
   ensure    => present,
   provider  => git,
   source    => 'git://git.moodle.org/moodle.git',
@@ -46,6 +46,6 @@ exec { 'install_moodle':
   cwd       => "$moodle_root/admin/cli",
   creates   => "$moodle_root/config.php",
   path      => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
-  command   => "php install.php --chmod=0750 --lang=el --wwwroot=$moodle_url --dataroot=$data_root --dbuser=moodle --dbpass='$moodle_db_pwd' --fullname='$moodle_fullname' --shortname='$moodle_shortname' --non-interactive --agree-license --adminpass='$moodle_adminpass'",
+  command   => "php install.php --chmod=0750 --lang=el --wwwroot=$moodle_root --dataroot=$data_root --dbuser=moodle --dbpass='$moodle_db_pwd' --fullname='$moodle_fullname' --shortname='$moodle_shortname' --non-interactive --agree-license --adminpass='$moodle_adminpass'",
   user      => 'www-data',
 }
