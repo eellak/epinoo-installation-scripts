92b4629ea630ebff2b501a4b39ed83a43298092c
Achilleas Pipinellis <axilleas@axilleas.me>
2015-11-07 14:34:48 +0200

    Change epinoo to wordpress

--- apache.pp
+++ apache.pp
@@ -1,55 +1,55 @@
-class { 'apache':  
+class { 'apache':
   mpm_module => 'prefork'
 }
 
 apache::listen { $listen_port: }
 apache::listen { $listen_ssl_port: }
 
-class {'apache::mod::php': } 
-class {'apache::mod::rewrite': } 
+class {'apache::mod::php': }
+class {'apache::mod::rewrite': }
 class {'apache::mod::shib':}
 
-apache::vhost { $epinoo:
-  ip              => $epinoo_ip,
+apache::vhost { $wordpress:
+  ip              => $wordpress_ip,
   port            => 80,
   add_listen      => false,
-  docroot         => $epinoo_root,
+  docroot         => $wordpress_root,
   docroot_owner   => $www_user,
   override        => ['All'],
   rewrites        => [{}],
   directories     => [
-    { 
+    {
     allow_override  => ['All'],
-    path            => $epinoo_root,
+    path            => $wordpress_root,
     },
   ],
 }
 
-apache::vhost { "$epinoo-ssl":
-  vhost_name      => $epinoo,
+apache::vhost { "$wordpress-ssl":
+  vhost_name      => $wordpress,
   ssl             => true,
   ssl_cert        => $moodle_ssl_cert,
   ssl_key         => $moodle_ssl_key,
-  ip              => $epinoo_ip,
+  ip              => $wordpress_ip,
   port            => 443,
   add_listen      => false,
-  docroot         => $epinoo_root,
+  docroot         => $wordpress_root,
   docroot_owner  => $www_user,
   override        => ['All'],
   rewrites        => [{}],
-  directories     => [ 
-    { 
+  directories     => [
+    {
     allow_override        => ['All'],
-    path                  => $epinoo_root,
+    path                  => $wordpress_root,
     auth_type             => 'Shibboleth',
     #shib_request_settings => { 'requiresession' => 'On' },
     shib_use_headers      => 'On',
     require               => 'shibboleth',
     },
-    { 
+    {
     auth_type             => 'Shibboleth',
     allow_override        => ['All'],
-    path                  => "$epinoo_root/secure/",
+    path                  => "$wordpress_root/secure/",
     shib_request_settings => { 'requiresession' => 'On' },
     shib_use_headers      => 'On',
     require               => 'valid-user',
@@ -63,14 +63,14 @@ apache::vhost { $moodle:
   add_listen      => false,
   docroot         => $moodle_root,
   docroot_owner  => 'root',
-  directories     => { 
+  directories     => {
     path => $moodle_root,
   }
 }
 
 
-class { 'shibboleth': 
-  hostname  => $epinoo,
+class { 'shibboleth':
+  hostname  => $wordpress,
 }
 
 shibboleth::metadata{ 'federation_metadata':
