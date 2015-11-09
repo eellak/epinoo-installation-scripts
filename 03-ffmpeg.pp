24d618e1e3bbbf81d108ca62c5fb61852088c644
Achilleas Pipinellis <axilleas@axilleas.me>
2015-11-09 11:22:26 +0200

    Try to run ffmpeg.sh with puppet

--- 03-ffmpeg.pp
+++ 03-ffmpeg.pp
@@ -1,5 +1,11 @@
+exec { 'ffmpeg_sh':
+  timeout => 0,
+  command => 'sh ffmpeg.sh',
+  creates => 'src/ffmpeg_2.3.3-1_amd64.deb',
+}
+
 package { "ffmpeg":
  provider => dpkg,
  ensure   => installed,
- source   => "ffmpeg_2.3.3-1_amd64.deb",
+ source   => "src/ffmpeg_2.3.3-1_amd64.deb",
 }
