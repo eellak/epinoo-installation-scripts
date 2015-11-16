f5c08b22abf5459563d977a29e48df64d7112b25
Athanasios Douitsis <aduitsis@cpan.org>
2015-11-16 13:31:51 +0200

    add dalexop key in keys.pp

--- keys.pp
+++ keys.pp
@@ -33,3 +33,10 @@ ssh_authorized_key { 'angela-user':
 	type	=> 'ssh-rsa',
 	key	=> 'AAAAB3NzaC1yc2EAAAABIwAAAQEAzkR6sRjsXUltrFSWNrvGpNy/t/WTY2yIwSmWWRSXJswxu4o/N/wZVVDBCJ3plTV6Ite9t9VnlYPYgqDM5QvFz41tTSl8aiJYJqdmX5/ysUyM5CAWDxgQZK5ih08IFL03Kz9q/M031aeU5zLrXz81ylYeG2cTRpZPPGdFfUyQzJcnvk4cNFPfF4QPC0LEMyYLycUFZho8vvkWhRivvVop2xZDSnpLTFbR+6OEoJ5Ro/uUrCcgGTRCGIVkWHBvduj/6TT4viPmYx12vS3mspeV9dDSSYb+HLNIf6/p13qrhqkIbFNnqFQi+ekQ4c4+BAA81cd9Zj0ExaKIo7nTBZsuCw=='
 }
+
+
+ssh_authorized_key { 'dalexop':
+	user	=> 'root',
+	type	=> 'ssh-rsa',
+	key	=> 'AAAAB3NzaC1yc2EAAAABJQAAAQEAiKQa4wuj76ul1LY9qopoJsBHKBdx6Tx2HVKS14FEmU2fwO4qAjPd1C85CuHNKKcyo4WLI86rg+wpuDGqUKPcDq6OOTpb+DPq8M/gua91C9q1lcfF3YwdJXeB9EqfffSkDYsExNwt0hFveXlDjpED0ZcoXtUSnLLrUwJbdo5xhB06gWBUgW28X6VHHWajzCou2Dcjyzn3fPJtlWP9Q4yGVnix6RFBm4/qGPxaDGheXn/mOBp0D0TQaCq4bzPChBbZyDXmibyvy+E6YBuQzAA632qK8Yu6A4UzEQDL2Ra8DDq/jnwgP9vowSz9cjKEilcGbg8td01fWDsfg2ry8Mgixw=='
+}
